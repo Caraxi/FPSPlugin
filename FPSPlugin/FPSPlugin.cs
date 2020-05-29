8f11ba87b53c4bcdaabc180d5c722c0be51a96b4
Caraxi <git@caraxian.com>
2020-05-30 07:31:54 +0930

    Initial Version

---
+++ FPSPlugin/FPSPlugin.cs
@@ -0,0 +1,122 @@
+﻿using Dalamud.Game.Internal.Network;
+using Dalamud.Plugin;
+using EasyHook;
+using ImGuiNET;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace FPSPlugin {
+	public class FPSPlugin : IDalamudPlugin {
+
+		public string Name => "FPS Plugin";
+		public DalamudPluginInterface PluginInterface { get; private set; }
+		public FPSPluginConfig PluginConfig { get; private set; }
+
+		private bool drawConfigWindow = false;
+
+		private long[] history;
+		private int c = 0;
+		private double lastFps = 0;
+		private int MAX_SIZE = 30;
+		private bool GameUIHidden = false;
+		private Stopwatch sw;
+
+		public void Dispose() {
+			PluginInterface.UiBuilder.OnBuildUi -= this.BuildUI;
+			PluginInterface.UiBuilder.OnOpenConfigUi -= this.OnConfigCommandHandler;
+			PluginInterface.Framework.Network.OnNetworkMessage -= this.OnNetworkHandler;
+			RemoveCommands();
+		}
+
+		public void Initialize(DalamudPluginInterface pluginInterface) {
+			this.PluginInterface = pluginInterface;
+			this.PluginConfig = (FPSPluginConfig)pluginInterface.GetPluginConfig() ?? new FPSPluginConfig();
+			this.PluginConfig.Init(this, pluginInterface);
+
+			history = new long[MAX_SIZE];
+			sw = new Stopwatch();
+			sw.Start();
+
+			SetupCommands();
+
+			PluginInterface.UiBuilder.OnBuildUi += this.BuildUI;
+			PluginInterface.UiBuilder.OnOpenConfigUi += OnConfigCommandHandler;
+			PluginInterface.Framework.Network.OnNetworkMessage += this.OnNetworkHandler;
+		}
+
+		private void OnNetworkHandler(IntPtr dataPtr, ushort opCode, uint sourceActorId, uint targetActorId, NetworkMessageDirection direction) {
+			// https://github.com/karashiiro/PingPlugin
+			const ushort eventPlay = 0x02C3;
+			const ushort eventFinish = 0x0239;
+			if (opCode == eventPlay)
+            {
+                var packetData = Marshal.PtrToStructure<EventPlay>(dataPtr);
+                
+                if ((packetData.Flags & 0x00000400) != 0)
+                    GameUIHidden = true;
+            }
+            else if (opCode == eventFinish)
+            {
+                GameUIHidden = false;
+            }
+		}
+
+		public void SetupCommands() {
+
+			PluginInterface.CommandManager.AddHandler("/pfps", new Dalamud.Game.Command.CommandInfo(OnConfigCommandHandler) {
+				HelpMessage = $"Open config window for {this.Name}",
+				ShowInHelp = true
+			});
+
+		}
+
+
+		public void OnConfigCommandHandler(object a, object b) {
+			drawConfigWindow = true;
+		}
+
+		public void RemoveCommands() {
+			PluginInterface.CommandManager.RemoveHandler("/pfps");
+
+		}
+
+		private void BuildUI() {
+			
+			long t = sw.ElapsedTicks;
+
+			history[c++] = t;
+
+			if (c == MAX_SIZE) {
+				c = 0;
+				lastFps = 10000000 / history.Average();
+			}
+
+			sw.Restart();
+			drawConfigWindow = drawConfigWindow && PluginConfig.DrawConfigUI();
+			if (!(GameUIHidden && PluginConfig.HideInCutscene)) {
+
+				ImGui.SetNextWindowBgAlpha(PluginConfig.Alpha);
+
+				ImGuiWindowFlags flags = ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoCollapse;
+
+				if (PluginConfig.Locked) {
+					flags |= ImGuiWindowFlags.NoMouseInputs | ImGuiWindowFlags.NoMove;
+				}
+
+				ImGui.Begin("FPS", flags);
+				if (PluginConfig.ShowDecimals) {
+					ImGui.TextColored(PluginConfig.Colour, $"FPS: {lastFps:F2}");
+				} else {
+					ImGui.TextColored(PluginConfig.Colour, $"FPS: {lastFps:F0}");
+				}
+
+				ImGui.End();
+
+			}
+			
+		}
+	}
+
+}
---
+++ FPSPlugin/FPSPluginConfig.cs
@@ -0,0 +1,96 @@
+﻿using CheapLoc;
+using Dalamud.Configuration;
+using Dalamud.Plugin;
+using ImGuiNET;
+using System;
+using System.Numerics;
+
+namespace FPSPlugin {
+	public class FPSPluginConfig : IPluginConfiguration {
+
+		[NonSerialized]
+		private DalamudPluginInterface pluginInterface;
+
+		[NonSerialized]
+		private FPSPlugin plugin;
+
+		public int Version { get; set; }
+
+		public bool Locked { get; set; }
+
+		public float Alpha { get; set; }
+
+		public bool ShowDecimals { get; set; }
+
+		public Vector4 Colour { get; set; }
+
+		public bool HideInCutscene { get; set; }
+
+		public FPSPluginConfig() {
+			LoadDefaults();
+		}
+
+		public void LoadDefaults() {
+			Colour = new Vector4(0, 1, 1, 1);
+			Alpha = 0.5f;
+			Locked = false;
+			HideInCutscene = true;
+			ShowDecimals = false;
+		}
+
+		public void Init(FPSPlugin plugin, DalamudPluginInterface pluginInterface) {
+			this.plugin = plugin;
+			this.pluginInterface = pluginInterface;
+		}
+
+		public void Save() {
+			pluginInterface.SavePluginConfig(this);
+		}
+
+		public bool DrawConfigUI() {
+			bool drawConfig = true;
+			ImGuiWindowFlags windowFlags = ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoCollapse;
+			ImGui.Begin($"{plugin.Name} Config", ref drawConfig, windowFlags);
+
+			bool locked = Locked;
+			if (ImGui.Checkbox("Lock Display", ref locked)) {
+				Locked = locked;
+				Save();
+			}
+			bool hideInCutscene = HideInCutscene;
+			if (ImGui.Checkbox("Hide during cutscenes", ref hideInCutscene)) {
+				HideInCutscene = hideInCutscene;
+				Save();
+			}
+
+			bool decimals = ShowDecimals;
+			if (ImGui.Checkbox("Show Decimals", ref decimals)) {
+				ShowDecimals = decimals;
+				Save();
+			}
+
+			float bgAlpha = Alpha;
+			if (ImGui.SliderFloat("Background Opacity", ref bgAlpha, 0, 1)) {
+				Alpha = Math.Max(0, Math.Min(1, bgAlpha));
+				Save();
+			}
+
+			Vector4 colour = Colour;
+			if (ImGui.ColorEdit4("Text Colour", ref colour)) {
+				Colour = colour;
+				Save();
+			}
+
+			ImGui.Separator();
+			if (ImGui.Button("Restore Default")) {
+				LoadDefaults();
+				Save();
+			}
+
+			ImGui.End();
+
+			return drawConfig;
+		}
+
+	}
+}
