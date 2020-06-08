2e1704d92daf37d8b936ad542f63458e52322660
Caraxi <git@caraxian.com>
2020-06-08 12:42:42 +0930

    Cleanup & Full UI Hide Detection & Possible Crash Prevention

--- FPSPlugin/FPSPlugin.cs
+++ FPSPlugin/FPSPlugin.cs
@@ -7,153 +7,196 @@ using System.Diagnostics;
 using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices;
+using Dalamud.Hooking;
 
 namespace FPSPlugin {
-	public class FPSPlugin : IDalamudPlugin {
-		public string Name => "FPS Plugin";
-		public DalamudPluginInterface PluginInterface { get; private set; }
-		public FPSPluginConfig PluginConfig { get; private set; }
+    public class FPSPlugin : IDalamudPlugin {
+        public string Name => "FPS Plugin";
+        public DalamudPluginInterface PluginInterface { get; private set; }
+        public FPSPluginConfig PluginConfig { get; private set; }
 
-		private bool drawConfigWindow = false;
-		private bool gameUIHidden = false;
+        private bool drawConfigWindow = false;
+        private bool gameUIHidden = false;
+        private bool chatHidden = false;
 
-		[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-		private delegate IntPtr GetBaseUIObjDelegate();
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate IntPtr GetBaseUIObjDelegate();
 
-		[UnmanagedFunctionPointer(CallingConvention.ThisCall, CharSet = CharSet.Ansi)]
-		private delegate IntPtr GetUI2ObjByNameDelegate(IntPtr getBaseUIObj, string uiName, int index = 1);
+        [UnmanagedFunctionPointer(CallingConvention.ThisCall, CharSet = CharSet.Ansi)]
+        private delegate IntPtr GetUI2ObjByNameDelegate(IntPtr getBaseUIObj, string uiName, int index = 1);
 
-		private GetBaseUIObjDelegate getBaseUIObj;
-		private GetUI2ObjByNameDelegate getUI2ObjByName;
+        private GetBaseUIObjDelegate getBaseUIObj;
+        private GetUI2ObjByNameDelegate getUI2ObjByName;
 
-		private IntPtr chatLogObject;
+        private delegate IntPtr ToggleUIDelegate(IntPtr baseAddress, byte unknownByte);
 
-		private List<float> fpsHistory;
-		private Stopwatch fpsHistoryInterval;
-		private string fpsText;
-		private Vector2 windowSize = Vector2.One;
+        private Hook<ToggleUIDelegate> toggleUIHook;
 
-		public void Dispose() {
-			PluginInterface.UiBuilder.OnBuildUi -= this.BuildUI;
-			PluginInterface.UiBuilder.OnOpenConfigUi -= this.OnConfigCommandHandler;
-			PluginInterface.Framework.OnUpdateEvent -= this.OnFrameworkUpdate;
-			fpsHistoryInterval?.Stop();
-			getBaseUIObj = null;
-			getUI2ObjByName = null;
-			RemoveCommands();
-			PluginInterface.Dispose();
-		}
+        private IntPtr chatLogObject;
 
-		public void Initialize(DalamudPluginInterface pluginInterface) {
-			this.PluginInterface = pluginInterface;
-			this.PluginConfig = (FPSPluginConfig)pluginInterface.GetPluginConfig() ?? new FPSPluginConfig();
-			this.PluginConfig.Init(this, pluginInterface);
-			fpsText = string.Empty;
-			fpsHistory = new List<float>();
-			fpsHistoryInterval = new Stopwatch();
-			fpsHistoryInterval.Start();
+        private List<float> fpsHistory;
+        private Stopwatch fpsHistoryInterval;
+        private string fpsText;
+        private Vector2 windowSize = Vector2.One;
 
-			SetupCommands();
+        public void Dispose() {
+            PluginInterface.UiBuilder.OnBuildUi -= this.BuildUI;
+            PluginInterface.UiBuilder.OnOpenConfigUi -= this.OnConfigCommandHandler;
+            PluginInterface.Framework.OnUpdateEvent -= this.OnFrameworkUpdate;
+            fpsHistoryInterval?.Stop();
+            getBaseUIObj = null;
+            getUI2ObjByName = null;
+            toggleUIHook?.Disable();
+            RemoveCommands();
+            PluginInterface.Dispose();
+        }
 
-			PluginInterface.UiBuilder.OnBuildUi += this.BuildUI;
-			PluginInterface.UiBuilder.OnOpenConfigUi += OnConfigCommandHandler;
-			PluginInterface.Framework.OnUpdateEvent += OnFrameworkUpdate;
+        public void Initialize(DalamudPluginInterface pluginInterface) {
+            this.PluginInterface = pluginInterface;
+            this.PluginConfig = (FPSPluginConfig) pluginInterface.GetPluginConfig() ?? new FPSPluginConfig();
+            this.PluginConfig.Init(this, pluginInterface);
+            fpsText = string.Empty;
+            fpsHistory = new List<float>();
+            fpsHistoryInterval = new Stopwatch();
+            fpsHistoryInterval.Start();
 
-			IntPtr getBaseUIObjScan = PluginInterface.TargetModuleScanner.ScanText("E8 ?? ?? ?? ?? 41 b8 01 00 00 00 48 8d 15 ?? ?? ?? ?? 48 8b 48 20 e8 ?? ?? ?? ?? 48 8b cf");
-			IntPtr getUI2ObjByNameScan = PluginInterface.TargetModuleScanner.ScanText("e8 ?? ?? ?? ?? 48 8b cf 48 89 87 ?? ?? 00 00 e8 ?? ?? ?? ?? 41 b8 01 00 00 00");
-			this.getBaseUIObj = Marshal.GetDelegateForFunctionPointer<GetBaseUIObjDelegate>(getBaseUIObjScan);
-			this.getUI2ObjByName = Marshal.GetDelegateForFunctionPointer<GetUI2ObjByNameDelegate>(getUI2ObjByNameScan);
-			this.chatLogObject = this.getUI2ObjByName(Marshal.ReadIntPtr(this.getBaseUIObj(), 32), "ChatLog", 1);
-		}
+            SetupCommands();
 
-		private void OnFrameworkUpdate(Framework framework) {
-			try {
-				if (fpsHistoryInterval.ElapsedMilliseconds > 1000) {
-					fpsHistoryInterval.Restart();
-					// FPS values are only updated in memory once per second.
-					float fps = Marshal.PtrToStructure<float>(PluginInterface.Framework.Address.BaseAddress + 0x165C);
-					fpsText = PluginConfig.ShowDecimals ? $"FPS: {fps:F2}" : $"FPS: {fps:F0}";
-					if (PluginConfig.ShowAverage || PluginConfig.ShowMinimum) {
-						fpsHistory.Add(fps);
-						if (fpsHistory.Count > PluginConfig.HistorySnapshotCount) {
-							fpsHistory.RemoveRange(0, fpsHistory.Count - PluginConfig.HistorySnapshotCount);
-						}
+            PluginInterface.UiBuilder.OnBuildUi += this.BuildUI;
+            PluginInterface.UiBuilder.OnOpenConfigUi += OnConfigCommandHandler;
+            PluginInterface.Framework.OnUpdateEvent += OnFrameworkUpdate;
 
-						if (PluginConfig.ShowAverage) {
-							fpsText += PluginConfig.ShowDecimals ? $" / Average: {fpsHistory.Average():F2}" : $" / Average: {fpsHistory.Average():F0}";
-						}
-				
-						if (PluginConfig.ShowMinimum) {
-							fpsText += PluginConfig.ShowDecimals ? $" / Min: {fpsHistory.Min():F2}" : $" / Min: {fpsHistory.Min():F0}";
-						}
-					}
-
-					windowSize = Vector2.Zero;
-				}
-				
-
-				// https://github.com/karashiiro/PingPlugin
-				if (this.PluginInterface.ClientState.LocalPlayer == null) {
-					gameUIHidden = false;
-					this.chatLogObject = IntPtr.Zero;
-					return;
-				}
-
-				if (chatLogObject == IntPtr.Zero) {
-					this.chatLogObject =
-						this.getUI2ObjByName(Marshal.ReadIntPtr(this.getBaseUIObj(), 32), "ChatLog", 1);
-					return;
-				}
-
-				gameUIHidden = Marshal.ReadByte(Marshal.ReadIntPtr(this.chatLogObject, 200) + 115) == 0;
-			} catch (Exception ex) {
-				PluginLog.LogError(ex.Message);
-				gameUIHidden = false;
-				this.chatLogObject = IntPtr.Zero;
-			}
-		}
-
-		public void SetupCommands() {
-			PluginInterface.CommandManager.AddHandler("/pfps", new Dalamud.Game.Command.CommandInfo(OnConfigCommandHandler) {
-				HelpMessage = $"Open config window for {this.Name}",
-				ShowInHelp = true
-			});
-		}
+            var getBaseUIObjScan = PluginInterface.TargetModuleScanner.ScanText("E8 ?? ?? ?? ?? 41 b8 01 00 00 00 48 8d 15 ?? ?? ?? ?? 48 8b 48 20 e8 ?? ?? ?? ?? 48 8b cf");
+            var getUI2ObjByNameScan = PluginInterface.TargetModuleScanner.ScanText("e8 ?? ?? ?? ?? 48 8b cf 48 89 87 ?? ?? 00 00 e8 ?? ?? ?? ?? 41 b8 01 00 00 00");
+            this.getBaseUIObj = Marshal.GetDelegateForFunctionPointer<GetBaseUIObjDelegate>(getBaseUIObjScan);
+            this.getUI2ObjByName = Marshal.GetDelegateForFunctionPointer<GetUI2ObjByNameDelegate>(getUI2ObjByNameScan);
+            this.chatLogObject = this.getUI2ObjByName(Marshal.ReadIntPtr(this.getBaseUIObj(), 32), "ChatLog", 1);
 
 
-		public void OnConfigCommandHandler(object a, object b) {
-			if (b is string s && s == "toggle") {
-				PluginConfig.Enable = !PluginConfig.Enable;
-				PluginConfig.Save();
-			} else {
-				drawConfigWindow = true;
-			}
-		}
+            var toggleUiPtr = pluginInterface.TargetModuleScanner.ScanText("48 89 5C 24 ?? 48 89 74 24 ?? 57 48 83 EC 20 0F B6 B9 ?? ?? ?? ?? B8 ?? ?? ?? ??");
+            toggleUIHook = new Hook<ToggleUIDelegate>(toggleUiPtr, new ToggleUIDelegate(((ptr, b) => {
+                gameUIHidden = (Marshal.ReadByte(ptr, 104008) & 4) == 0;
+                return toggleUIHook.Original(ptr, b);
+            })));
 
-		public void RemoveCommands() {
-			PluginInterface.CommandManager.RemoveHandler("/pfps");
-		}
+            toggleUIHook.Enable();
+        }
 
-		private void BuildUI() {
-			drawConfigWindow = drawConfigWindow && PluginConfig.DrawConfigUI();
-			if (!(gameUIHidden && PluginConfig.HideInCutscene) && PluginConfig.Enable && !string.IsNullOrEmpty(fpsText)) {
-				ImGui.SetNextWindowBgAlpha(PluginConfig.Alpha);
+        private void OnFrameworkUpdate(Framework framework) {
+            try {
+                if (fpsHistoryInterval.ElapsedMilliseconds > 1000) {
+                    fpsHistoryInterval.Restart();
+                    // FPS values are only updated in memory once per second.
+                    var fps = Marshal.PtrToStructure<float>(PluginInterface.Framework.Address.BaseAddress + 0x165C);
+                    fpsText = PluginConfig.ShowDecimals ? $"FPS: {fps:F2}" : $"FPS: {fps:F0}";
+                    if (PluginConfig.ShowAverage || PluginConfig.ShowMinimum) {
+                        fpsHistory.Add(fps);
+                        if (fpsHistory.Count > PluginConfig.HistorySnapshotCount) {
+                            fpsHistory.RemoveRange(0, fpsHistory.Count - PluginConfig.HistorySnapshotCount);
+                        }
 
-				ImGuiWindowFlags flags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoCollapse;
+                        if (PluginConfig.ShowAverage) {
+                            fpsText += PluginConfig.ShowDecimals ? $" / Average: {fpsHistory.Average():F2}" : $" / Average: {fpsHistory.Average():F0}";
+                        }
 
-				if (PluginConfig.Locked) {
-					flags |= ImGuiWindowFlags.NoMouseInputs | ImGuiWindowFlags.NoMove;
-				}
+                        if (PluginConfig.ShowMinimum) {
+                            fpsText += PluginConfig.ShowDecimals ? $" / Min: {fpsHistory.Min():F2}" : $" / Min: {fpsHistory.Min():F0}";
+                        }
+                    }
 
-				if (windowSize == Vector2.Zero) {
-					windowSize = ImGui.CalcTextSize(fpsText) + (ImGui.GetStyle().WindowPadding * 2);
-				}
+                    windowSize = Vector2.Zero;
+                }
 
-				ImGui.SetNextWindowSize(windowSize, ImGuiCond.Always);
-				ImGui.Begin("FPS##fpsPluginMonitorWindow", flags);
-				ImGui.TextColored(PluginConfig.Colour, fpsText);
-				ImGui.End();
-			}
-		}
-	}
+
+                // https://github.com/karashiiro/PingPlugin
+                if (this.PluginInterface.ClientState.LocalPlayer == null) {
+                    chatHidden = false;
+                    this.chatLogObject = IntPtr.Zero;
+                    return;
+                }
+
+                if (chatLogObject == IntPtr.Zero) {
+                    this.chatLogObject =
+                        this.getUI2ObjByName(Marshal.ReadIntPtr(this.getBaseUIObj(), 32), "ChatLog", 1);
+                    return;
+                }
+
+                var chatLogProperties = Marshal.ReadIntPtr(this.chatLogObject, 0xC8);
+                if (chatLogProperties == IntPtr.Zero) {
+                    this.chatHidden = true;
+                    return;
+                }
+
+                chatHidden = Marshal.ReadByte(chatLogProperties + 0x73) == 0;
+            } catch (Exception ex) {
+                PluginLog.LogError(ex.Message);
+                chatHidden = false;
+                this.chatLogObject = IntPtr.Zero;
+            }
+        }
+
+        public void SetupCommands() {
+            PluginInterface.CommandManager.AddHandler("/pfps", new Dalamud.Game.Command.CommandInfo(OnConfigCommandHandler) {
+                HelpMessage = $"Open config window for {this.Name}. /pfps [show|hide|toggle|reset]",
+                ShowInHelp = true
+            });
+        }
+
+
+        public void OnConfigCommandHandler(object a, object b) {
+            if (b is string s) {
+                switch (s.ToLower()) {
+                    case "toggle": {
+                        PluginConfig.Enable = !PluginConfig.Enable;
+                        break;
+                    }
+                    case "show": {
+                        PluginConfig.Enable = true;
+                        break;
+                    }
+                    case "hide": {
+                        PluginConfig.Enable = false;
+                        break;
+                    }
+                    case "reset": {
+                        fpsHistory.Clear();
+                        break;
+                    }
+                    default: {
+                        drawConfigWindow = true;
+                        break;
+                    }
+                }
+
+                PluginConfig.Save();
+            } else {
+                drawConfigWindow = true;
+            }
+        }
+
+        public void RemoveCommands() {
+            PluginInterface.CommandManager.RemoveHandler("/pfps");
+        }
+
+        private void BuildUI() {
+            drawConfigWindow = drawConfigWindow && PluginConfig.DrawConfigUI();
+            if ((gameUIHidden || chatHidden) && PluginConfig.HideInCutscene || !PluginConfig.Enable || string.IsNullOrEmpty(fpsText)) return;
+            ImGui.SetNextWindowBgAlpha(PluginConfig.Alpha);
+
+            var flags = ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoCollapse;
+
+            if (PluginConfig.Locked) {
+                flags |= ImGuiWindowFlags.NoMouseInputs | ImGuiWindowFlags.NoMove;
+            }
+
+            if (windowSize == Vector2.Zero) {
+                windowSize = ImGui.CalcTextSize(fpsText) + (ImGui.GetStyle().WindowPadding * 2);
+            }
+
+            ImGui.SetNextWindowSize(windowSize, ImGuiCond.Always);
+            ImGui.Begin("FPS##fpsPluginMonitorWindow", flags);
+            ImGui.TextColored(PluginConfig.Colour, fpsText);
+            ImGui.End();
+        }
+    }
 }
--- FPSPlugin/FPSPluginConfig.cs
+++ FPSPlugin/FPSPluginConfig.cs
@@ -5,133 +5,133 @@ using System;
 using System.Numerics;
 
 namespace FPSPlugin {
-	public class FPSPluginConfig : IPluginConfiguration {
-		[NonSerialized] private DalamudPluginInterface pluginInterface;
+    public class FPSPluginConfig : IPluginConfiguration {
+        [NonSerialized] private DalamudPluginInterface pluginInterface;
 
-		[NonSerialized] private FPSPlugin plugin;
+        [NonSerialized] private FPSPlugin plugin;
 
-		public int Version { get; set; }
+        public int Version { get; set; }
 
-		public bool Locked { get; set; }
+        public bool Locked { get; set; }
 
-		public float Alpha { get; set; }
+        public float Alpha { get; set; }
 
-		public bool ShowDecimals { get; set; }
+        public bool ShowDecimals { get; set; }
 
-		public Vector4 Colour { get; set; }
+        public Vector4 Colour { get; set; }
 
-		public bool HideInCutscene { get; set; }
+        public bool HideInCutscene { get; set; }
 
-		public bool Enable { get; set; }
+        public bool Enable { get; set; }
 
-		public int HistorySnapshotCount { get; set; }
+        public int HistorySnapshotCount { get; set; }
 
-		public bool ShowAverage { get; set; }
-		public bool ShowMinimum { get; set; }
+        public bool ShowAverage { get; set; }
+        public bool ShowMinimum { get; set; }
 
-		public FPSPluginConfig() {
-			LoadDefaults();
-		}
+        public FPSPluginConfig() {
+            LoadDefaults();
+        }
 
-		public void LoadDefaults() {
-			Colour = new Vector4(0, 1, 1, 1);
-			Alpha = 0.5f;
-			Locked = false;
-			HideInCutscene = true;
-			ShowDecimals = false;
-			HistorySnapshotCount = 1;
-			ShowAverage = false;
-		}
+        public void LoadDefaults() {
+            Colour = new Vector4(0, 1, 1, 1);
+            Alpha = 0.5f;
+            Locked = false;
+            HideInCutscene = true;
+            ShowDecimals = false;
+            HistorySnapshotCount = 300;
+            ShowAverage = false;
+        }
 
-		public void Init(FPSPlugin plugin, DalamudPluginInterface pluginInterface) {
-			this.plugin = plugin;
-			this.pluginInterface = pluginInterface;
-		}
+        public void Init(FPSPlugin plugin, DalamudPluginInterface pluginInterface) {
+            this.plugin = plugin;
+            this.pluginInterface = pluginInterface;
+        }
 
-		public void Save() {
-			pluginInterface.SavePluginConfig(this);
-		}
+        public void Save() {
+            pluginInterface.SavePluginConfig(this);
+        }
 
-		public bool DrawConfigUI() {
-			bool drawConfig = true;
-			ImGuiWindowFlags windowFlags = ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoCollapse;
-			ImGui.Begin($"{plugin.Name} Config##fpsPluginConfigWindow", ref drawConfig, windowFlags);
+        public bool DrawConfigUI() {
+            bool drawConfig = true;
+            ImGuiWindowFlags windowFlags = ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoCollapse;
+            ImGui.Begin($"{plugin.Name} Config##fpsPluginConfigWindow", ref drawConfig, windowFlags);
 
-			bool enabled = Enable;
-			if (ImGui.Checkbox("Show Display##fpsPluginEnabledSetting", ref enabled)) {
-				Enable = enabled;
-				Save();
-			}
+            bool enabled = Enable;
+            if (ImGui.Checkbox("Show Display##fpsPluginEnabledSetting", ref enabled)) {
+                Enable = enabled;
+                Save();
+            }
 
-			ImGui.SameLine();
+            ImGui.SameLine();
 
-			ImGui.TextColored(new Vector4(0.6f, 0.6f, 0.6f, 1), "/pfps toggle");
+            ImGui.TextColored(new Vector4(0.6f, 0.6f, 0.6f, 1), "/pfps [show|hide|toggle]");
 
-			bool locked = Locked;
-			if (ImGui.Checkbox("Lock Display##fpsPluginLockSetting", ref locked)) {
-				Locked = locked;
-				Save();
-			}
+            bool locked = Locked;
+            if (ImGui.Checkbox("Lock Display##fpsPluginLockSetting", ref locked)) {
+                Locked = locked;
+                Save();
+            }
 
-			bool hideInCutscene = HideInCutscene;
-			if (ImGui.Checkbox("Hide during cutscenes##fpsPluginHideSetting", ref hideInCutscene)) {
-				HideInCutscene = hideInCutscene;
-				Save();
-			}
+            bool hideInCutscene = HideInCutscene;
+            if (ImGui.Checkbox("Hide when chat is hidden##fpsPluginHideSetting", ref hideInCutscene)) {
+                HideInCutscene = hideInCutscene;
+                Save();
+            }
 
-			bool decimals = ShowDecimals;
-			if (ImGui.Checkbox("Show Decimals##fpsPluginDecimalsSetting", ref decimals)) {
-				ShowDecimals = decimals;
-				Save();
-			}
+            bool decimals = ShowDecimals;
+            if (ImGui.Checkbox("Show Decimals##fpsPluginDecimalsSetting", ref decimals)) {
+                ShowDecimals = decimals;
+                Save();
+            }
 
-			bool showAverage = ShowAverage;
-			if (ImGui.Checkbox("Show Average##fpsPluginShowAverageSetting", ref showAverage)) {
-				ShowAverage = showAverage;
-				Save();
-			}
+            bool showAverage = ShowAverage;
+            if (ImGui.Checkbox("Show Average##fpsPluginShowAverageSetting", ref showAverage)) {
+                ShowAverage = showAverage;
+                Save();
+            }
 
-			bool showMin = ShowMinimum;
-			if (ImGui.Checkbox("Show Minimum##fpsPluginShowMinimumSetting", ref showMin)) {
-				ShowMinimum = showMin;
-				Save();
-			}
+            bool showMin = ShowMinimum;
+            if (ImGui.Checkbox("Show Minimum##fpsPluginShowMinimumSetting", ref showMin)) {
+                ShowMinimum = showMin;
+                Save();
+            }
 
-			float bgAlpha = Alpha;
-			if (ImGui.SliderFloat("Background Opacity##fpsPluginOpacitySetting", ref bgAlpha, 0, 1)) {
-				Alpha = Math.Max(0, Math.Min(1, bgAlpha));
-				Save();
-			}
+            float bgAlpha = Alpha;
+            if (ImGui.SliderFloat("Background Opacity##fpsPluginOpacitySetting", ref bgAlpha, 0, 1)) {
+                Alpha = Math.Max(0, Math.Min(1, bgAlpha));
+                Save();
+            }
 
-			int historySnapshotCount = HistorySnapshotCount;
-			if (ImGui.InputInt("Tracking Timespan (Seconds)", ref historySnapshotCount, 1, 60)) {
-				if (historySnapshotCount < 1) {
-					historySnapshotCount = 1;
-				}
+            int historySnapshotCount = HistorySnapshotCount;
+            if (ImGui.InputInt("Tracking Timespan (Seconds)", ref historySnapshotCount, 1, 60)) {
+                if (historySnapshotCount < 1) {
+                    historySnapshotCount = 1;
+                }
 
-				if (historySnapshotCount > 10000) {
-					historySnapshotCount = 10000;
-				}
+                if (historySnapshotCount > 10000) {
+                    historySnapshotCount = 10000;
+                }
 
-				HistorySnapshotCount = historySnapshotCount;
-				Save();
-			}
+                HistorySnapshotCount = historySnapshotCount;
+                Save();
+            }
 
-			Vector4 colour = Colour;
-			if (ImGui.ColorEdit4("Text Colour##fpsPluginColorSetting", ref colour)) {
-				Colour = colour;
-				Save();
-			}
+            Vector4 colour = Colour;
+            if (ImGui.ColorEdit4("Text Colour##fpsPluginColorSetting", ref colour)) {
+                Colour = colour;
+                Save();
+            }
 
-			ImGui.Separator();
-			if (ImGui.Button("Restore Default##fpsPluginDefaultsButton")) {
-				LoadDefaults();
-				Save();
-			}
+            ImGui.Separator();
+            if (ImGui.Button("Restore Default##fpsPluginDefaultsButton")) {
+                LoadDefaults();
+                Save();
+            }
 
-			ImGui.End();
+            ImGui.End();
 
-			return drawConfig;
-		}
-	}
+            return drawConfig;
+        }
+    }
 }
